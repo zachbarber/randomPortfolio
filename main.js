ff338f16bbc21e11703cc469cc1d96ad7b1b4d52
Zach Barber <zachbarber321@gmail.com>
2019-10-11 23:53:49 -0400

    add random text API, add styling changes

--- main.js
+++ main.js
@@ -6,14 +6,43 @@ document.addEventListener("DOMContentLoaded", async () => {
     main(userData);
 });
 
-const main = (userData) => {
+const fetchAboutMe = async () => {
+    const fetchText = await fetch('https://www.randomtext.me/api/lorem');
+    if (fetchText.status != 200) return console.log(fetchText.message);
+    dataJson = fetchText.json()
+    return dataJson;
+}
+
+const main = async (userData) => {
+    console.log(userData);
     //DOM Elements
+
     //Header
     const headerName = document.getElementById('headerName');
-    
-    
+
+    //Content Wrapper
+    const contentWrapper = document.querySelector('.contentWrapper');
+
+    //DOM Elements
+
     //Header
     const userName = `${userData.name.first} ${userData.name.last}`;
     headerName.innerHTML = userName
+
+    //User Image
+    const userImg = document.createElement('img');
+    userImg.src = userData.picture.large;
+    userImg.className = 'userImg';
+    contentWrapper.appendChild(userImg);
+
+    //About Me
+    this.aboutMeText = await fetchAboutMe();
+    const aboutMeText = document.createElement('p');
+    aboutMeText.innerHTML = await this.aboutMeText.text_out;
+    aboutMeText.className = 'aboutMeText';
+    contentWrapper.appendChild(aboutMeText);
+
+
+
 }
 
